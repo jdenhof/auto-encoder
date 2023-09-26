ad472891a17f05dd2c4a384f25bc2d436d81f025
Jagger Denhof <denhofja@mail.gvsu.edu>
2023-09-26 13:19:19 -0400

    optimizer selection

--- AutoEncoderV3.py
+++ AutoEncoderV3.py
@@ -1,4 +1,5 @@
 # %%
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,17 +9,19 @@ import os
 import inspect
 from torchvision import datasets
 
+# Gather MNIST 
 train_set_tensor = datasets.MNIST('./data', train=True, download=True)
 test_set_tensor = datasets.MNIST('./data', train=False, download=True)
+# Convert to numpy
+raw_train_set = train_set_tensor.data.numpy()
+raw_test_set = test_set_tensor.data.numpy()
+# Normalize 
+raw_train_set = raw_train_set / 255.0
+raw_test_set = raw_test_set / 255.0
+# Reshape
+raw_train_set = raw_train_set.reshape(-1, 784)
+raw_test_set = raw_test_set.reshape(-1, 784)
 
-train_set = train_set_tensor.data.numpy()
-test_set = test_set_tensor.data.numpy()
-
-train_set = train_set / 255.0
-test_set = test_set / 255.0
-
-train_set = train_set.reshape(-1, 784)
-test_set = test_set.reshape(-1, 784)
 np.random.seed(42)
 
 def preview(image):
@@ -26,7 +29,7 @@ def preview(image):
     plt.axis('off') 
     plt.show()
 
-preview(test_set[0].reshape(28, 28))
+preview(raw_test_set[0].reshape(28, 28))
 # %%
 from abc import ABC, abstractmethod
 import numpy as np
@@ -37,8 +40,8 @@ class LossFunctions:
     @staticmethod 
     def MSE(input_batch, predicted):
         
-        loss = np.mean(np.square(predicted - input_batch))
-        gradient = predicted - input_batch
+        loss = np.mean(np.square(input_batch - predicted))
+        gradient = predicted - input_batch / np.prod(predicted.shape[:-1])
 
         return loss, gradient
     
@@ -124,7 +127,6 @@ class LinearLayer(Layer):
         return np.dot(self.input_batch, self.weights) + self.biases
 
     def backward(self, error_batch):
-
         self.error_batch = error_batch
         self.weights_error = np.dot(self.input_batch.T, error_batch)
         input_error = np.dot(error_batch, self.weights.T)
@@ -136,6 +138,7 @@ class LinearLayer(Layer):
         self.biases -= learning_rate * (np.sum(self.error_batch, axis = 0) / batch_size)
         
 
+
 # %%
 class AutoEncoder:
     
@@ -145,20 +148,23 @@ class AutoEncoder:
         decode_layers,
         epochs = 100,
         learning_rate = 0.1,
-        loss_function = LossFunctions.MSE
+        loss_function = LossFunctions.MSE,
+        optimizer = 'SGD'
     ):
 
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.loss_function = loss_function
+        self.optimizer = optimizer
         self.encoder = encode_layers
         self.decoder = decode_layers
         self.loss_graph = {}
     
     def forward(self, input_batch):
+        # Encode
         for layer in self.encoder:
             input_batch = layer.forward(input_batch)
-
+        # Decode
         for layer in self.decoder:
             input_batch = layer.forward(input_batch)
 
@@ -168,9 +174,101 @@ class AutoEncoder:
 
         for layer in reversed(self.decoder):
             error_batch = layer.backward(error_batch)
-        
+
         for layer in reversed(self.encoder):
             error_batch = layer.backward(error_batch)
+    
+    def train(self, train_set, batch_size = 1):
+        if self.optimizer == 'SGD':
+            self.train_sgd(train_set)
+        
+        if self.optimizer == 'Batch':
+            self.train_batch(train_set)
+
+        if self.optimizer == 'MiniBatch':
+            if batch_size == 1:
+                print("Warning: You are doinog MiniBatch with a batch_size of 1!")
+            self.train_minibatch(train_set, batch_size)
+
+        print(f"Error: No optimizer matched {self.optimizer}") 
+
+    def train_sgd(self, train_set):
+        epochs_time = 0
+        epochs_loss = 0
+        self.batch_size = 1
+        for epoch in range(self.epochs):
+            epoch_loss = 0
+            epoch_start = time.time()
+            for input_batch in train_set:
+                # input_batch = train_set[random.randint(0, len(train_set) - 1)]
+                predicted = self.forward(input_batch.reshape(1, -1))
+                loss, error_batch = self.loss_function(input_batch, predicted)
+                epoch_loss += loss
+
+                self.backward(error_batch)
+
+                self.update_all_layers()
+
+            self.loss_graph[epoch] = epoch_loss
+
+            epochs_loss += epoch_loss 
+            hours, mins, seconds, epoch_total, avg_time, epochs_time = self.epochtime(epoch, epoch_start, epochs_time)
+            
+            print(f"Epoch {epoch}, Loss: {epoch_loss:.4f} Avg Loss {epochs_loss / (epoch + 1):.4f} Time: {epoch_total:.2f} Avg Time: {avg_time:.2f} Estimated: {hours}:{mins}:{seconds} ")
+            self.display()
+
+    def update_all_layers(self):
+        for layer in self.decoder + self.encoder:
+            layer.update(self.learning_rate, self.batch_size)
+
+    def train_batch(self, train_batch):
+        epochs_loss = 0
+        epochs_time = 0
+        self.batch_size = len(train_batch)
+        for epoch in range(self.epochs):
+            epoch_start = time.time()
+            predicted = self.forward(train_batch)
+            loss, error_batch = self.loss_function(train_batch, predicted)
+
+            self.backward(error_batch)
+            
+            self.update_all_layers()
+
+            self.loss_graph[epoch] = loss
+            epochs_loss += loss 
+            hours, mins, seconds, epoch_total, avg_time, epochs_time = self.epochtime(epoch, epoch_start, epochs_time)
+        
+            print(f"Epoch {epoch}, Loss: {loss:.4f} Avg Loss {epochs_loss / (epoch + 1):.4f} Time: {epoch_total:.2f} Avg Time: {avg_time:.2f} Estimated: {hours}:{mins}:{seconds} ")
+
+    def train_minibatch(self, train_set, batch_size):
+        epochs_loss = 0
+        epochs_time = 0
+        self.batch_size = batch_size
+        batches = [train_set[i:i+batch_size] for i in range(0, train_set.shape[0], batch_size)]
+        for epoch in range(self.epochs):
+            epoch_loss = 0
+            batches = random.shuffle(batches)
+
+            for train_batch in batches:
+                epoch_start = time.time()
+                predicted = self.forward(train_batch)
+                
+                loss, error_batch = self.loss_function(train_batch, predicted)
+
+                self.backward(error_batch)
+
+                for layer in self.decoder + self.encoder:
+                    layer.update(self.learning_rate, self.batch_size)
+
+                epoch_loss += loss 
+            hours, mins, seconds, epoch_total, avg_time, epochs_time = self.epochtime(epoch, epoch_start, epochs_time)
+            
+            self.loss_graph[epoch] = epoch_loss
+            epochs_loss += epoch_loss
+
+            print(f"Epoch {epoch}, Loss: {epoch_loss:.4f} Avg Loss {epochs_loss / (epoch + 1):.4f} Time: {epoch_total:.2f} Avg Time: {avg_time:.2f} Estimated: {hours}:{mins}:{seconds} ")
+            if epoch != 0:
+                self.display()
 
     def epochtime(self, epoch, epoch_start, epochs_time):
 
@@ -186,96 +284,6 @@ class AutoEncoder:
         seconds = int((est_time - hours_in_secs* hours - min_in_secs*mins))
 
         return hours, mins, seconds, epoch_total, avg_time, epochs_time
-
-    def train_sgd(self, train_set):
-        epochs_time = 0
-        epochs_loss = 0
-        for epoch in range(self.epochs):
-            epoch_loss = 0
-            epoch_start = time.time()
-            for input_batch in train_set:
-
-                predicted = self.forward(input_batch.reshape(1, -1))
-                loss, error_batch = self.loss_function(input_batch, predicted)
-                epoch_loss += loss
-
-                self.backward(error_batch)
-
-                for layer in reversed(self.decoder):
-                    layer.update(self.learning_rate, self.batch_size)
-        
-                for layer in reversed(self.encoder):
-                    layer.update(self.learning_rate, self.batch_size)
-
-
-            self.loss_graph[epoch] = epoch_loss
-
-            epochs_loss += epoch_loss 
-            hours, mins, seconds, epoch_total, avg_time, epochs_time = self.epochtime(epoch, epoch_start, epochs_time)
-            
-            print(f"Epoch {epoch}, Loss: {epoch_loss:.4f} Avg Loss {epochs_loss / (epoch + 1):.4f} Time: {epoch_total:.2f} Avg Time: {avg_time:.2f} Estimated: {hours}:{mins}:{seconds} ")
-
-            if (epoch + 1 % 10 == 0):
-                self.display()
-
-    def train_batch(self, train_batch):
-        epochs_loss = 0
-        epochs_time = 0
-        self.batch_size = len(train_batch)
-        for epoch in range(self.epochs):
-            epoch_start = time.time()
-            predicted = self.forward(train_batch)
-            loss, error_batch = self.loss_function(train_set, predicted)
-
-            self.backward(error_batch)
-            
-            for layer in reversed(self.decoder):
-                    layer.update(self.learning_rate, self.batch_size)
-        
-            for layer in reversed(self.encoder):
-                layer.update(self.learning_rate, self.batch_size)
-
-            self.loss_graph[epoch] = loss
-            epochs_loss += loss 
-            hours, mins, seconds, epoch_total, avg_time, epochs_time = self.epochtime(epoch, epoch_start, epochs_time)
-        
-            print(f"Epoch {epoch}, Loss: {loss:.4f} Avg Loss {epochs_loss / (epoch + 1):.4f} Time: {epoch_total:.2f} Avg Time: {avg_time:.2f} Estimated: {hours}:{mins}:{seconds} ")
-
-    def train_minibatch(self, train_set, batch_size):
-        epochs_loss = 0
-        epochs_time = 0
-        self.batch_size = batch_size
-        for epoch in range(self.epochs):
-            batches = [train_set[i:i+batch_size, :] for i in range(0, train_set.shape[0], batch_size)]
-            epoch_loss = 0
-            prev_epoch_loss = 100000000000
-            for train_batch in batches:
-                epoch_start = time.time()
-                predicted = self.forward(train_batch)
-                
-                loss, error_batch = self.loss_function(train_batch, predicted)
-
-                self.backward(error_batch)
-
-                for layer in reversed(self.decoder):
-                    layer.update(self.learning_rate, self.batch_size)
-        
-                for layer in reversed(self.encoder):
-                    layer.update(self.learning_rate, self.batch_size)
-
-                epoch_loss += loss 
-                hours, mins, seconds, epoch_total, avg_time, epochs_time = self.epochtime(epoch, epoch_start, epochs_time)
-            
-            self.loss_graph[epoch] = epoch_loss
-            epochs_loss += epoch_loss
-
-            if epoch_loss > prev_epoch_loss:
-                return
-            
-            prev_epoch_loss = epoch_loss
-            print(f"Epoch {epoch}, Loss: {epoch_loss:.4f} Avg Loss {epochs_loss / (epoch + 1):.4f} Time: {epoch_total:.2f} Avg Time: {avg_time:.2f} Estimated: {hours}:{mins}:{seconds} ")
-            if epoch != 0:
-                self.display()
 
     def save(self):
         x = 0
@@ -298,23 +306,22 @@ class AutoEncoder:
         plt.show()
 
 # %%
+autoencoder = AutoEncoder(
+    encode_layers = [
+        LinearLayer(784, 256),
+        Sigmoid(),
+        LinearLayer(256, 32),
+        Sigmoid(),
+    ], 
+    decode_layers = [
+        LinearLayer(32, 256),
+        Sigmoid(),
+        LinearLayer(256, 784),
+        Sigmoid()
+    ], 
+    optimizer = 'SGD')
 
-encoder = [
-    LinearLayer(784, 256),
-    Sigmoid(),
-    LinearLayer(256, 32),
-    Sigmoid(),
-]
-
-decoder = [
-    LinearLayer(32, 256),
-    Sigmoid(),
-    LinearLayer(256, 784),
-    Sigmoid()
-]
-
-autoencoder = AutoEncoder(encode_layers = encoder, decode_layers = decoder)
-autoencoder.train_sgd(train_set)
+autoencoder.train(raw_train_set)
 
 # %%
 
@@ -324,12 +331,13 @@ def test_helper(autoencoder: AutoEncoder, start_point, end_point):
         return
 
     for test in range(start_point, end_point + 1):
-        forward_result = autoencoder.forward(test_set[test].reshape(1, 784))
-        preview(test_set[test].reshape(28, 28))
+        forward_result = autoencoder.forward(raw_test_set[test].reshape(1, 784))
+        preview(raw_test_set[test].reshape(28, 28))
         preview(forward_result.reshape(28, 28))
 
 def test(start_point, end_point):
     test_helper(autoencoder, start_point, end_point)
+
 # %%
 class tester:
 
@@ -342,8 +350,8 @@ class tester:
             return
 
         for test in range(start_point, end_point + 1):
-            forward_result = autoencoder.forward(test_set[test].reshape(1, 784))
-            preview(test_set[test].reshape(28, 28))
+            forward_result = autoencoder.forward(raw_test_set[test].reshape(1, 784))
+            preview(raw_test_set[test].reshape(28, 28))
             preview(forward_result.reshape(28, 28))
     
     def test(self, start_point, end_point):
@@ -364,9 +372,6 @@ class tester:
         except:
             pass
         
-        print("Loading autoencdoer failed!")
+        print("Loading autoencoder failed!")
             
-
-    
-
 # %%
