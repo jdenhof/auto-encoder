5ce2cf032c6c6a5e8ab8f108afaea890bba64347
kinardc <kinardchase@outlook.com>
2023-10-09 18:45:53 +0000

    cleaned up

--- ParamGridSearch.py
+++ ParamGridSearch.py
@@ -2,33 +2,43 @@ import random
 from AutoEncoderV3 import AutoEncoder, LinearLayer, Sigmoid
 
 
-def get_r():
+def rand_learning_rate():
     return random.uniform( 0.001, 0.01 )
 
-def get_batch_size():
+def rand_batch_size():
     return random.choice( [ 16, 32, 64, 128, 256, 512 ] )
 
-def get_n_epochs():
+def rand_n_epochs():
     return random.randint( 1, 10 )
 
-def get_layers():
-    n_hidden = random.randrange( 1, 10, 2 )
-    layer_sizes = [
+def rand_layer_sizes( n_hidden: int ):
+    return [
         784,
         *[ random.choice( [ 16, 32, 64, 128, 256, 512 ] ) for _ in range( n_hidden ) ],
         784
     ]
-    n_layers = n_hidden + 2
-    layers = [ LinearLayer( *layer_sizes[ i:i + 2 ] ) for i in range( n_layers - 1 ) ]    # Generate linear layers
-    layers = list( zip( layers, [ Sigmoid() ] * n_layers ) )   # Pair a sigmoid layer with each linear layer
-    layers = [ x for t in layers for x in t ]    # Flatten list of layers
+
+def rand_layers( n_hidden = None ):
+    if n_hidden == None:
+        n_hidden = random.randrange( 1, 10, 2 )
+    layer_sizes = rand_layer_sizes( n_hidden )
+    n_layers = len( layer_sizes ) - 1
+    layers = list(
+        x for t in    # Flatten list
+        [ zip(
+            [ LinearLayer( *layer_sizes[ i:i + 2 ] ) for i in range( n_layers ) ],    # Generate linear layers
+            [ Sigmoid() ] * n_layers    # Pair a sigmoid layer with each linear layer
+        ) ]
+        for x in t    # End flatten
+    )
     center = len( layers ) // 2
     return layers[ center: ], layers[ :center ]    # Split list into encoder and decoder sets
 
-encoder_layers, decoder_layers = get_layers()
-print( "Encoder Layers" )
-for l in encoder_layers:
-    print( l )
-print( "Decoder Layers" )
-for l in decoder_layers:
-    print( l )
+# encoder_layers, decoder_layers = rand_layers()
+# print( "Encoder Layers" )
+# for l in encoder_layers:
+#     print( l )
+# print( "Decoder Layers" )
+# for l in decoder_layers:
+#     print( l )
+
